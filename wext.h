5322ad444bd2fa3e93451055764c9d41615f5c8f
Patrick-Gilles Maillot <patrick.maillot@gmail.com>
2020-04-25 14:26:00 +0200

    Create wext.h
    v 0.1 commit

---
+++ wext.h
@@ -0,0 +1,28 @@
+/*
+ * wext.h
+ *
+ *  Created on: Apr 25, 2020
+ *      Author: Patrick-Gilles Maillot
+ */
+
+#ifndef WEXT_H_
+#define WEXT_H_
+
+extern int wKeepAlive();
+extern int wOpen(char* wip);
+extern int wClose();
+extern int wVer();
+
+extern type wGetType(wtoken token);
+extern char* wGetName(wtoken token);
+extern int wGetFloatToken(wtoken tokenval, float *fval);
+extern int wGetFloatTokenTimed(wtoken tokenval, float *fval, int timeout);
+extern int wGetIntTokenTimed(wtoken tokenval, int *ival, int timeout);
+extern int wGetStringTokenTimed(wtoken tokenval, char* str, int timeout);
+extern int wGetVoidPTokenTimed(wtoken *tokenval, void *vpt, int timeout);
+
+extern int wSetTokenFloat(wtoken tokenval, float fval);
+extern int wSetTokenInt(wtoken tokenval, int ival);
+extern int wSetTokenString(wtoken tokenval, char* str);
+
+#endif /* WEXT_H_ */
