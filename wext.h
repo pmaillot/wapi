df6477823bc84a2ffd5170b4f445f71b6293ef8a
Patrick-Gilles Maillot <patrick.maillot@gmail.com>
2020-04-26 19:31:30 +0200

    ver 0.2

--- wext.h
+++ wext.h
@@ -16,6 +16,8 @@ extern int wVer();
 extern type wGetType(wtoken token);
 extern char* wGetName(wtoken token);
 extern int wGetFloatToken(wtoken tokenval, float *fval);
+extern int wGetIntToken(wtoken tokenval, float *fval);
+extern int wGetStringToken(wtoken tokenval, float *fval);
 extern int wGetFloatTokenTimed(wtoken tokenval, float *fval, int timeout);
 extern int wGetIntTokenTimed(wtoken tokenval, int *ival, int timeout);
 extern int wGetStringTokenTimed(wtoken tokenval, char* str, int timeout);
@@ -24,5 +26,8 @@ extern int wGetVoidPTokenTimed(wtoken *tokenval, void *vpt, int timeout);
 extern int wSetTokenFloat(wtoken tokenval, float fval);
 extern int wSetTokenInt(wtoken tokenval, int ival);
 extern int wSetTokenString(wtoken tokenval, char* str);
+
+extern int wGetNode(wtoken node, char *str);
+extern int wGetNodeTimed(wtoken node, char *str, int timeout);
 
 #endif /* WEXT_H_ */
