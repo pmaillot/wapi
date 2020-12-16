ede02cf86d18669da4d22f39c83c9544a99b7ab9
Patrick-Gilles Maillot <patrick.maillot@gmail.com>
2020-12-16 08:58:32 +0100

    Update wext.h

--- wext.h
+++ wext.h
@@ -32,7 +32,7 @@ extern int wSetTokenInt(wtoken tokenval, int ival);
 extern int wSetTokenString(wtoken tokenval, char* str);
 //
 extern int wSetNode(char *str);
-extern int wSetNodeFromTVArray(wTV *TV, int nTV);
+extern int wSetNodeFromTVArray(wTV *array, int nTV);
 extern int wGetNode(wtoken node, char *str);
 extern int wGetNodeToTVArray(wtoken node, wTV *array);
 //
