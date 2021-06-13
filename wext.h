8400ccc7efd5c414aed0a05b55f9ace17ac8531b
Patrick-Gilles Maillot <patrick.maillot@gmail.com>
2021-06-13 17:46:55 +0200

    ver 1.0

--- wext.h
+++ wext.h
@@ -15,21 +15,25 @@ extern int wMeterUDPPort(int wport);
 //
 extern wtype wGetType(wtoken token);
 extern char* wGetName(wtoken token);
-extern int wGetToken(wtoken tokenval, wtype *type, wvalue *value);
-extern int wGetFloatToken(wtoken tokenval, float *fval);
-extern int wGetIntToken(wtoken tokenval, int *ival);
-extern int wGetVoidPToken(wTV *tv);
 //
-extern int wGetStringToken(wtoken tokenval, char* str);
+extern int wGetTokenDef(wtoken token, int *num, unsigned char* *str);
+//
+extern int wGetToken(wtoken tokenval, wtype *type, wvalue *value);
+extern int wGetTokenFloat(wtoken tokenval, float *fval);
+extern int wGetTokenInt(wtoken tokenval, int *ival);
+extern int wGetTokenString(wtoken tokenval, char* str);
+extern int wGetParsedEvents(wTV *tv);
+//
 extern int wGetTokenTimed(wtoken tokenval, wtype *type, wvalue *value, int timeout);
-extern int wGetFloatTokenTimed(wtoken tokenval, float *fval, int timeout);
-extern int wGetIntTokenTimed(wtoken tokenval, int *ival, int timeout);
-extern int wGetStringTokenTimed(wtoken tokenval, char* str, int timeout);
-extern int wGetVoidPTokenTimed(wTV *tv, int timeout);
+extern int wGetTokenFloatTimed(wtoken tokenval, float *fval, int timeout);
+extern int wGetTokenIntTimed(wtoken tokenval, int *ival, int timeout);
+extern int wGetTokenStringTimed(wtoken tokenval, char* str, int timeout);
+extern int wGetParsedEventsTimed(wTV *tv, int timeout);
 //
 extern int wSetTokenFloat(wtoken tokenval, float fval);
 extern int wSetTokenInt(wtoken tokenval, int ival);
 extern int wSetTokenString(wtoken tokenval, char* str);
+
 //
 extern int wSetNode(char *str);
 extern int wSetNodeFromTVArray(wTV *array, int nTV);
