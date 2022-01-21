ea0b87f1ab884e70f0b56a0cbe040510564b65a5
Patrick-Gilles Maillot <patrick.maillot@gmail.com>
2022-01-21 18:53:10 +0100

    Updated to avoid memory leak

--- wevents.c
+++ wevents.c
@@ -11,6 +11,7 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 //
 #include "../wapi/wapi.h"
 #include "../wapi/wext.h"
@@ -22,29 +23,41 @@ int main() {
 	char*			Ntoken = 0;			// pointer to token name
 	unsigned int	Otoken = 0;			// token previous value
 	int				events;				// number of events (or error code)
-	wTV				TV[128];			// we should get less than 128 events every 100us
+	wTV				TV[512];			// we should get less than 512 events every 100us
 
+	printf("Using wapi ver: %i.%i\n", wVer() >> 8, wVer() & 0xFF);
 	if ((events = wOpen(wingip)) != WSUCCESS) return(-1);
 	printf("WING found at IP: %s\n", wingip);
-	printf("Using wapi ver: %i.%i\n", wVer() >> 8, wVer() & 0xFF);
 
 	while (1) {
 		wKeepAlive();
 		//
 		if ((events = wGetParsedEventsTimed(TV, 500)) > 0) {
+			if (events > 480) {
+				printf("events= %d\n", events);
+				fflush (stdout);
+			}
 			for (int i = 0; i < events; i++) {
-				if (TV[i].token != Otoken) {			// Could call wGetName() at each
-					Otoken = TV[i].token;				// iteration, but this should
-					Ntoken = wGetName(TV[i].token);		// save a little resource time
+				if (TV[i].token != $SYSCFG_CONSOLENAME) {
+					if (TV[i].token != Otoken) {			// Could call wGetName() at each
+						Otoken = TV[i].token;				// iteration, but this should
+						Ntoken = wGetName(TV[i].token);		// save a little resource time
+					}
+					printf("W-> %s data = ", Ntoken);
+					if (TV[i].type == I32) printf("%i\n", TV[i].d.idata);
+					else if (TV[i].type == F32) printf("%.2f\n", TV[i].d.fdata);
+					else {
+						if (TV[i].d.sdata) {				// should then be S32
+							printf("%s\n", TV[i].d.sdata);
+							fflush(stdout);
+						} else {
+							printf("\n");
+							fflush(stdout);
+						}
+					}
 				}
-				printf("W-> %s data = ", Ntoken);
-				if (TV[i].type == I32) printf("%i\n", TV[i].d.idata);
-				else if (TV[i].type == F32) printf("%.2f\n", TV[i].d.fdata);
-				else if (TV[i].d.sdata) {				// should then be S32
-					printf("%s\n", TV[i].d.sdata);
-					free(TV[i].d.sdata);				// No memory leak
-				}
-				fflush(stdout);
+				// Avoid memory leaks
+				if (TV[i].type == S32) free(TV[i].d.sdata);
 			}
 		} else {
 			if (events != WZERO) {
